eeb4f88aa7113ed2eed3a23aaa687347685aafac
Rachelle Herrera <rachelle.herera@gmail.com>
2016-02-03 21:11:27 -0800

    require models and contollers in the app.js file

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
+var http = require('http'); 
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var session = require('express-session'); 
+var MongoStore = require('connect-mongo')(session); 
+var flash = require('connect-flash'); 
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override'); 
+var mongoose = require('mongoose'); 
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy; 
+var routes = require('./routes/index'); 
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static('/public')); 
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,7 +28,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride)('_method')); 
+
+app.use(express.static(path.join(_dirname, 'public')));
+app.listen(process.env.PORT || 3000); 
+
+/* source in models */
+var User = require('./models/User'); 
 
 app.use('/', routes);
 app.use('/users', users);
