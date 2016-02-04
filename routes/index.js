ca3d06f1606106d39cd8b87ac91d366c84ee0e6e
Rachelle Herrera <rachelle.herera@gmail.com>
2016-02-03 23:05:35 -0800

    transfer file user routing in main routing file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+/* passport middleware will add authenticated users */
+var passport = require('passport'); 
+var methodOverride = require('method-override'); 
 
+/* required controllers */
+var SessionsController = require('../controllers/Sessions'); 
+var UsersController    = require('../controllers/Users'); 
+
+/* checks if the user is logged in */
+var isLoggedIn = function(req, res, next) { 
+  if (!req.isAuthenticated()) { 
+    res.redirect('/login'); 
+  }
+    return next(); 
+}; 
+
+/* renders sessions controller */
+router.get('/login', SessionsController.sessionsNew); 
+router.post('/login', passport.authenticate(
+  'local', 
+  { 
+    failureRedirect: '/login'
+  });                 SessionsController.sessionCreate); 
+router.get('/logout', SessionsController.sessionsDelete); 
+
+
+/* renders users controller */
+router.get('/auth/register', UserController.UsersNew); 
+router.post('/auth/register', UserController.UsersCreate);
+router.get('/users', isLoggedIn, UserController.UsersIndex); 
+router.get('/users/:id', isLoggedIn, UserController.UserShow);
+router.get('/users/:id/edit', isLoggedIn, UserController.userEdit); 
+router.put('/users/:id', isLoggedIn, UserController.userUpdate); 
+router.delete('/users/:id', isLoggedIn, UserController.UserDelete); 
+
+/* Export Module */
 module.exports = router;
