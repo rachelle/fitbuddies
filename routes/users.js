0c257f0ba1f1955f649f2c2eeb60ab68e6e954a7
Rachelle Herrera <rachelle.herera@gmail.com>
2016-02-03 20:35:37 -0800

    added passport and method-override package for user authentication

---
+++ routes/users.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+/* passport middleware will add authenticated users */
+var passport = require('passport'); 
+var methodOverride = require('method-override'); 
+
+module.exports = router;
