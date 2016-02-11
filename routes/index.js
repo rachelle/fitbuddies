3a47a281c09b35655e2ea1b7ab395de0b02b6b5d
Rachelle Herrera <rachelle.herera@gmail.com>
2016-02-10 22:31:52 -0800

    added register form to register view page

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express  = require('express');
 var router   = express.Router();
+var Account = require('../models/account'); 
+var router = express.Router(); 
 
 /* passport middleware will add authenticated users */
 var passport       = require('passport'); 
